063666ec9f98ea9e8f24a72bdb9f0408dc66b60e
fmmdevs <fmm.devs@gmail.com>
2024-08-30 07:58:14 +0200

    No More Waving Flags: End Threads

---
+++ devs/fmm/localedatetime/threads/createthreads/NoMoreWavingFlags.java
@@ -0,0 +1,65 @@
+package devs.fmm.localedatetime.threads.createthreads;
+
+import javax.swing.*;
+
+public class NoMoreWavingFlags {
+    public static void main(String[] args) {
+
+        Runnable winkFlag = () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("wink");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    System.out.println("winkThread interrupted (during sleep)");
+                    Thread.currentThread().interrupt();
+
+                }
+            }
+        };
+
+        Runnable waveFlag = () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("wave flag");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    System.out.println("waveThread interrupted (during sleep)");
+                    Thread.currentThread().interrupt();
+                }
+
+            }
+        };
+
+
+        Thread winkThread = new Thread(winkFlag);
+        Thread waveThread = new Thread(waveFlag);
+        winkThread.start();
+        waveThread.start();
+
+        String endInput;
+
+        do {
+            endInput = JOptionPane.showInputDialog("endw to stop winking.\nendf to stop flag waving.\n");
+            if (endInput.equals("endf")) {
+                waveThread.interrupt();
+            }
+            if (endInput.equals("endw")) {
+                winkThread.interrupt();
+            }
+            if (waveThread.isInterrupted() && endInput.equals("continuef")) {
+                waveThread.start();
+            }
+            if (waveThread.isInterrupted() && endInput.equals("continuew")) {
+                winkThread.start();
+            }
+        } while (!endInput.equals("exit"));
+
+        System.exit(0);
+
+    }
+
+}
+
